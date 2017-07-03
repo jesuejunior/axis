4831f39a35bd87cc518d2a2fa10deb91dfc50e68
Jesue Junior <jesuesousa@gmail.com>
2017-07-03 00:25:39 -0300

    Adding first tests

---
+++ test/services.spec.js
@@ -0,0 +1,3 @@
+// Criar testes de serviço, quando fosse necessario ter logica dentro dos serviços
+// Por enquanto é "garantido"pela biblioteca mongoose
+// não vejo necessidade de  cria rum mock para verificar se uma chamada está sendo feita.
