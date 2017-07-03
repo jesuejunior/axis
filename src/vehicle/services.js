54020cb8e742ba8e1226334cdc3f56d5f4ea4847
Jesue Junior <jesuesousa@gmail.com>
2017-07-03 03:25:56 -0300

    Adding services

---
+++ src/vehicle/services.js
@@ -0,0 +1,37 @@
+const logger = require('../toolbox/logger');
+const Vehicle = require('./models');
+
+class VehicleService {
+  getAll() {
+    logger.info('Getting all vehicles');
+    return Vehicle.find().lean().exec();
+  }
+
+  create(data) {
+    const vehicle = new Vehicle(data);
+    return vehicle.save();
+  }
+
+  findById(id) {
+    const vehicle = Vehicle.findById(id).lean().exec();
+    return vehicle;
+  }
+  search(param) {
+    const vehicles = Vehicle.where('vehicle').equals(param).exec();
+    return vehicles;
+  }
+
+  remove(id) {
+    const vehicle = Vehicle.findByIdAndRemove(id).exec();
+    return vehicle;
+  }
+
+  update(id, data) {
+    const vehicle = Vehicle.findOneAndUpdate({ _id: id }, data, (err, doc) => {
+      logger.info('updating vehicle', data, 'to', doc);
+    }).exec();
+    return vehicle;
+  }
+}
+
+module.exports = VehicleService;
